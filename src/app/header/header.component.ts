d8226bd9a58e049903a884d2af2e1ad5cb7057a3
7roubb <7roubb@gmail.com>
2024-03-10 16:01:16 +0200

    commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component,ElementRef, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+ isOpen:boolean = false;
+ toggleBtnIconClass: string = 'fa-solid fa-bars';
+
+  
+
+ toggleDropDown(){
+   console.log('toggleDropDown');
+   this.isOpen = !this.isOpen;
+   this.toggleBtnIconClass = this.isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+
+
+ constructor(private elementRef: ElementRef) {}
+
+
+ @HostListener('document:click', ['$event'])
+ onClick(event: Event) {
+   if (!this.elementRef.nativeElement.contains(event.target)) {
+     this.isOpen = false;
+     this.toggleBtnIconClass = this.isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+
+   }
+ }
+}
